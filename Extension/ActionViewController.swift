eda8424c966fa88bfe3849ae1cbc16603d44ce58
GilCND <fgilcnd@gmail.com>
2021-08-30 16:50:38 -0300

    [project19-SafariExtension-01] Implemented JavaScript Injection

---
+++ Extension/ActionViewController.swift
@@ -0,0 +1,48 @@
+//
+//  ActionViewController.swift
+//  Extension
+//
+//  Created by Felipe Gil on 2021-08-30.
+//
+
+import UIKit
+import MobileCoreServices
+
+class ActionViewController: UIViewController {
+    @IBOutlet weak var script: UITextView!
+    var pageTitle = ""
+    var pageURL = ""
+    
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector (done))
+        
+        if let inputItem = extensionContext?.inputItems.first as? NSExtensionItem {
+            if let itemProvider = inputItem.attachments?.first {
+                itemProvider.loadItem(forTypeIdentifier: kUTTypeBinaryPropertyList as String) { [weak self] (dict, error) in
+                    guard let itemDictionary = dict as? NSDictionary else { return }
+                    guard let javaScriptValues = itemDictionary [NSExtensionJavaScriptPreprocessingResultsKey] as? NSDictionary else { return }
+                    self?.pageTitle = javaScriptValues["title"] as? String ?? ""
+                    self?.pageURL = javaScriptValues["URL"] as? String ?? ""
+                    
+                    DispatchQueue.main.async {
+                        self?.title = self?.pageTitle
+                    }
+                }
+            }
+        }
+    }
+
+    @IBAction func done() {
+        let item = NSExtensionItem()
+        let argument: NSDictionary = ["customJavaScript": script.text]
+        let webDictionary: NSDictionary = [NSExtensionJavaScriptFinalizeArgumentKey: argument]
+        let customJavaScript = NSItemProvider(item: webDictionary, typeIdentifier: kUTTypePropertyList as String)
+        item.attachments = [customJavaScript]
+
+        extensionContext?.completeRequest(returningItems: [item])
+    }
+
+}
